7b88fef466afcb048097d00d11f2dc88dafc9853
Sonam Phuntsog <spsither@gmail.com>
2025-04-24 16:36:17 -0400

    test: embed in SquareSpace

---
+++ app/slider/page.js
@@ -0,0 +1,15 @@
+import BeforeAfterSlider from '@/components/BeforeAfterSlider';
+export default function Page() {
+    return (
+
+        <BeforeAfterSlider
+            before="/images/Ganden - 1963 - U2.jpg"
+            after="/images/Ganden - 1976 - Hexagon.png"
+            alt="Before and After Comparison"
+            description="Ganden 1963 U2 vs 1976 after the cultural revolution"
+            beforeLabel='1963'
+            afterLabel='1976'
+        />
+
+    );
+}
